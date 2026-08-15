9e51c093bda02bd24c39a9ad0ac34e5a66e3e503
Lucas <lucassellart@gmail.com>
2026-08-15 19:36:28 -0300

    correccion estructura de paquetes

---
+++ backend/src/main/java/com/proyecto/ticket/GestionTicketsApplication.java
@@ -0,0 +1,12 @@
+package com.proyecto.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionTicketsApplication.class, args);
+    }
+}
